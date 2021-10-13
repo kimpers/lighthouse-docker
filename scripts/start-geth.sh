ce86ec21cf112eb91f340c96d9b83c851ada5715
Kim Persson <kimpersson88@gmail.com>
2021-10-13 10:39:35 +0200

    fix: enable geth snap sync mode

--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -14,6 +14,6 @@ elif [ "$START_GETH" != "" ]; then
 	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
 		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable
 	else
-		exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable
+		exec geth --syncmode snap --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable
 	fi
 fi
